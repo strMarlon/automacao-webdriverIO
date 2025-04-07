33cd61e4ffcbf0a4967ca51791c1f6d0fba0e457
Marlon Amorim <sddamata38@gmail.com>
2025-04-07 11:43:24 -0300

    Atualizacao no browserStack

--- configs/browserstack/wdio.android.bs.conf.js
+++ configs/browserstack/wdio.android.bs.conf.js
@@ -5,7 +5,7 @@ export const config = {
     key: process.env.BROWSERSTACK_ACCESS_KEY,
 
     specs: [
-        'tests/forms/forms.test.js'
+        './tests/forms/forms.test.js'
     ],
 
     // Patterns to exclude.
